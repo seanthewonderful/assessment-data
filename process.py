833951627b4c351ba7a452e14a57294d6d0c2022
Sean <seanthewonderful@gmail.com>
2022-03-11 10:05:20 -0700

    part1 commit

--- process.py
+++ process.py
@@ -4,11 +4,12 @@ log_file = open("um-server-01.txt")
 
 def sales_reports(log_file): # Creating a function(def=defining) called sales_report taking in a parameter, "log_file"
     for line in log_file: # Indent to run as the function. Iterate through each line in log_file and do the following:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+        line = line.rstrip() # Removing the white space at the end (right side) of each line string
+        day = line[0:3] # Setting variable day = the first 3 characters of each line. Starts at string position 0, prints 3 characters.
+        # if day == "Tue": # Creating condition if those first 3 characters match "Tue", then do the following
+        if day == "Mon": # New code per directions to print sales info for Mon instead
+            print(line) # Print the entire line to the console.
 
 
-sales_reports(log_file)
+sales_reports(log_file) # Invoking the function with the file "log_file" as its parameter, through which this function will be able to iterate
 
