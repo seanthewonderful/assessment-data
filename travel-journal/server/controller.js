a08ca7e1e37fd1b6893ac8da3cebc0737cd521b9
Sean <seanthewonderful@gmail.com>
2022-03-11 11:30:05 -0700

    step6 commit

--- travel-journal/server/controller.js
+++ travel-journal/server/controller.js
@@ -246,5 +246,15 @@ module.exports = {
             VALUES ('${name}', ${rating}, ${countryId})
         `).then(dbRes => res.status(200).send(dbRes[0]))
         .catch(err => console.log(err))
+    },
+    getCities: (req, res) => {
+        sequelize.query(`
+        SELECT cities.city_id, cities.name AS city, countries.country_id, countries.name AS country
+        FROM cities
+        JOIN countries
+            ON cities.country_id = countries.country_id
+        `)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
     }
 }
